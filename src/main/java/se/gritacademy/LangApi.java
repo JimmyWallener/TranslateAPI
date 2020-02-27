44a2d1628b9284bcb21375e01592f5107cf244ce
Jimmy Wallener <38855083+jimwdev@users.noreply.github.com>
2020-02-27 16:04:26 +0100

    Updated with error handling
    Added error handling with use of defaultMessage. Tested and should work as intended now.

--- src/main/java/se/gritacademy/LangApi.java
+++ src/main/java/se/gritacademy/LangApi.java
@@ -20,40 +20,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LangApi {
 			
+			private String notFound = "Error: Not found";
+			private String errorInput = "Error: String empty OR contain Integers";
 			
 			@Autowired
 			private MessageSource messageSource;
 			private LangBean bean = new LangBean();
+			
 
 			
 			
 			// Creating a RequestParameter for 'lang' which will set output language
 			// word/Hello?lang=countrycode and translate individual word
 			// Doing a whitespace trim and set to lowercase for matching value in properties file.
+			
 			@RequestMapping(path = "/word-translation/{word}", method = RequestMethod.GET, produces = "application/json")
 			public String translateWord(@RequestParam(name = "lang", required = false) Locale locale,
 				@PathVariable String word) throws NoSuchMessageException{
 				String pattern = "{\"%s\"}";
-				String json = String.format(pattern, messageSource.getMessage(word.trim().toLowerCase(), null, locale));
+				String json = String.format(pattern, messageSource.getMessage(word.trim().toLowerCase(), null, notFound, locale));
+				String invalid = String.format(pattern, errorInput);
 				if (Controller.isValid(word)) {
 					return json;
-				} else {
-					return "Error: " + word + " is not valid! Letters only";
-				}
+				} return invalid;
 			}
 			
 			
 			// RequestHeader for phrases - Works for translating a website with setting Accept-Language.
+			
 			@RequestMapping(path = "/{phrases}", method = RequestMethod.GET, produces = "application/json")
 			public String getPhrases(@RequestHeader(name = "Accept-Language", required = false) Locale locale,
 					@PathVariable String phrases) throws NoSuchMessageException {
 				String pattern = "{\"%s\"}";
-				String json = String.format(pattern, messageSource.getMessage(phrases.trim().toLowerCase(), null, locale));
+				String json = String.format(pattern, messageSource.getMessage(phrases.trim().toLowerCase(), null,notFound, locale));
+				String invalid = String.format(pattern, errorInput);
 				if (Controller.isValid(phrases)) {
 					return json;
-				} else {
-					return "Error: " + phrases + " is not valid! Letters only";
-				}
+				} return invalid;
 			}
 			
 			
@@ -62,38 +65,29 @@ public class LangApi {
 			@RequestMapping(path = "/get-word", method = RequestMethod.GET, produces = "application/json")
 			public String getWords(@RequestHeader(name = "Accept-Language", required = false) Locale locale) throws NoSuchMessageException{
 				String pattern = "{\"%s\": \"%s\"}"; 
-				//String altPattern = "{\"%s\"}";
-				// String isNull = messageSource.getMessage(bean.getWord(), null, bean.getWord(), locale);
+				String altPattern = "{\"%s\"}";
+				boolean wordCheck = messageSource.getMessage(bean.getWord(), null, notFound, locale).equals(notFound);
 				
 				String json = String.format(pattern, bean.getWord(),bean.getTranslatedWord()); 
-			//	String messagesJson	= String.format(altPattern, messageSource.getMessage(bean.getWord(), null, locale));
+				String messagesJson	= String.format(altPattern, messageSource.getMessage(bean.getWord(), null, notFound, locale));
 				 
 				
-//				if(isNull.equals(bean.getWord())) {
-//					return messagesJson;
-//				} else {
-//					return json;
-//				}
+				if(wordCheck) {
+					return json;
+				} else {
+					return messagesJson;
+				}
 				
-//				try {
-//					if (isNull) {
-//						return messagesJson;
-//					}
-//				} catch (NoSuchMessageException e) {
-//					return newWordsJson;
-//				}
-//				return null;
-
-				return json;
 			}
 			
 			
 			// POST Mapping for setting a single word or phrase. Just checks that it's not empty or integers
-			@RequestMapping(path = "/add-words/{word}/{translatedWord}", method = RequestMethod.POST)
-			public void postWord(@PathVariable String word, @PathVariable String translatedWord) throws Exception {
+			@RequestMapping(path = "/add-words/{word}/{translatedWord}", method = RequestMethod.POST, produces = "application/json")
+			public String postWord(@PathVariable String word, @PathVariable String translatedWord) throws Exception {
 				bean.addOwnWords(word, translatedWord);
-				
-				
+				String pattern = "{\"%s\": \"%s\"}"; 
+				String json = String.format(pattern, word, translatedWord);
+				return json;
 			}
 			
 
